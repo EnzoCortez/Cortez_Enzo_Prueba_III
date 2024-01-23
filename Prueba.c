900185e6e6cb47d1070a558511041a0f10e28240
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-01-23 14:27:08 -0500

    Definicion de las funciones para la manipulación de archivos

--- Prueba.c
+++ Prueba.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*Definicion de la estructura basica*/
+// Definición de la estructura alumno
 struct alumno
 {
     int matricula;
@@ -9,3 +9,10 @@ struct alumno
     char materia[50];
     float nota;
 };
+
+// funciones para la manipulacion de archivos
+void ingresarAlumnos(struct alumno lista[], int n);
+void mostrarAlumnos(struct alumno lista[], int n);
+void guardarEnArchivo(struct alumno lista[], int n);
+void leerDesdeArchivo(struct alumno lista[], int n);
+
