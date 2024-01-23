d05ad80b2cc96801d19b32a8224071fb7a3f9423
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-01-23 14:29:39 -0500

    Definicion del menu y creacion de un arreglo de estructura para la manipulacion del archivo

--- Prueba.c
+++ Prueba.c
@@ -16,3 +16,55 @@ void mostrarAlumnos(struct alumno lista[], int n);
 void guardarEnArchivo(struct alumno lista[], int n);
 void leerDesdeArchivo(struct alumno lista[], int n);
 
+int main()
+{
+    int opcion;
+    int n;
+
+    // Solicitar al usuario la cantidad de alumnos
+    printf("Ingrese la cantidad de alumnos: ");
+    scanf("%d", &n);
+
+    // Crear un arreglo de estructuras alumno con capacidad para n alumnos
+    struct alumno listaAlumnos[n];
+
+    // Cargar datos desde el archivo al inicio del programa
+    leerDesdeArchivo(listaAlumnos, n);
+
+    do
+    {
+        // Menú principal
+        printf("\nMenú:\n");
+        printf("1. Ingresar alumnos\n");
+        printf("2. Mostrar alumnos\n");
+        printf("3. Guardar en archivo\n");
+        printf("4. Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            ingresarAlumnos(listaAlumnos, n);
+            break;
+
+        case 2:
+            mostrarAlumnos(listaAlumnos, n);
+            break;
+
+        case 3:
+            guardarEnArchivo(listaAlumnos, n);
+            break;
+
+        case 4:
+            printf("Guardando en archivo y saliendo del programa.\n");
+            guardarEnArchivo(listaAlumnos, n);
+            break;
+
+        default:
+            printf("Opción no válida. Intente de nuevo.\n");
+        }
+    } while (opcion != 4);
+
+    return 0;
+}
